e1fc569d0cd9445b87163f0272ac67de9fa6b1e6
LubicBydgoszcz <bekisz43@gmail.com>
2023-03-01 15:01:25 +0100

    poprawki

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    Vector2 movementVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementVector = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * movementVector.x);
+        transform.Translate(Vector3.forward* movementVector.y * Time.deltaTime);
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        movementVector = inputValue.Get<Vector2>();
+
+        Debug.Log(movementVector.ToString());
     }
 }

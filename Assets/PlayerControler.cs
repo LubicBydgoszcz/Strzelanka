f2a23f4656472312a69a4de956be77e8614d7259
LubicBydgoszcz <bekisz43@gmail.com>
2023-03-06 15:22:17 +0100

    nadgoniłem

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -6,24 +6,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20;
+    public float playerSpeed = 2;
     Vector2 movementVector;
+    Transform bulletSpawn;
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
+        bulletSpawn = transform.Find("BulletSpawn");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * movementVector.x);
-        transform.Translate(Vector3.forward* movementVector.y * Time.deltaTime);
+        transform.Translate(Vector3.forward* movementVector.y * Time.deltaTime * playerSpeed);
     }
 
     void OnMove(InputValue inputValue)
     {
         movementVector = inputValue.Get<Vector2>();
+    }
 
-        Debug.Log(movementVector.ToString());
+    void OnFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn);
+        bullet.transform.parent = null;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*bulletSpeed, ForceMode.VelocityChange);
+        Destroy(bullet, 5);
     }
 }
---
+++ Assets/ZombieControler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieControler : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+        Vector3 playerDirection = transform.position- player.transform.position;
+        transform.Translate(playerDirection.normalized *Time.deltaTime);
+    }
+}
